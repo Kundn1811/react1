3348f3ee0aad80b1f5cbc5163ce755c5eee33cba
Kundan <kd181197@gmail.com>
2022-05-16 19:45:53 +0530

    first commit

---
+++ src/component/mobile.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+let Body1 = ()=>{
+
+    return (
+        <div>
+          <h1>Mobile Operating System</h1>
+          <ul>
+            <li>
+              Android
+            </li>
+            <li>Blackberry</li>
+            <li>iPhone</li>
+            <li>Window Phone</li>
+          </ul>
+
+          <h1>Mobile Manufacturers</h1>
+          <ul id="list">
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+          </ul>
+        </div>
+      );
+}
+
+export {Body1}
